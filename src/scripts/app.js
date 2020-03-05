b0b4273e47b8cb10a35d14c8eebd0171ac597601
Clara Pasteau <clarapasteau@gmail.com>
2020-03-05 13:00:05 +0100

    implementing section expertise actualite contact

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,42 @@
 import "../sass/app.scss";
 
+$(document).ready(function(){
+
+    // expertise 1
+    $(".exper__droits--detail1").hide();
+    $(".exper__droits__list--right--arrow1").click(function(){
+        $(".exper__droits--detail1").toggle(500);
+        $(".exper__droits__list--right--arrow1").hide();
+      });
+
+      $(".exper__droits--reduire1").click(function(){
+        $(".exper__droits--detail1").toggle(500);
+        $(".exper__droits__list--right--arrow1").show();
+      });
+
+
+    // expertise 2
+    $(".exper__droits--detail2").hide();
+    $(".exper__droits__list--right--arrow2").click(function(){
+        $(".exper__droits--detail2").toggle(500);
+        $(".exper__droits__list--right--arrow2").hide();
+      });
+
+      $(".exper__droits--reduire2").click(function(){
+        $(".exper__droits--detail2").toggle(500);
+        $(".exper__droits__list--right--arrow2").show();
+      });
+
+    // expertise 3
+    $(".exper__droits--detail3").hide();
+    $(".exper__droits__list--right--arrow3").click(function(){
+        $(".exper__droits--detail3").toggle(500);
+        $(".exper__droits__list--right--arrow3").hide();
+      });
+
+      $(".exper__droits--reduire3").click(function(){
+        $(".exper__droits--detail3").toggle(500);
+        $(".exper__droits__list--right--arrow3").show();
+      });
+  
+  });
--- web/build/dev/app.js
+++ web/build/dev/app.js
@@ -193,6 +193,39 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("../sass/app.scss");
+
+$(document).ready(function () {
+  // expertise 1
+  $(".exper__droits--detail1").hide();
+  $(".exper__droits__list--right--arrow1").click(function () {
+    $(".exper__droits--detail1").toggle(500);
+    $(".exper__droits__list--right--arrow1").hide();
+  });
+  $(".exper__droits--reduire1").click(function () {
+    $(".exper__droits--detail1").toggle(500);
+    $(".exper__droits__list--right--arrow1").show();
+  }); // expertise 2
+
+  $(".exper__droits--detail2").hide();
+  $(".exper__droits__list--right--arrow2").click(function () {
+    $(".exper__droits--detail2").toggle(500);
+    $(".exper__droits__list--right--arrow2").hide();
+  });
+  $(".exper__droits--reduire2").click(function () {
+    $(".exper__droits--detail2").toggle(500);
+    $(".exper__droits__list--right--arrow2").show();
+  }); // expertise 3
+
+  $(".exper__droits--detail3").hide();
+  $(".exper__droits__list--right--arrow3").click(function () {
+    $(".exper__droits--detail3").toggle(500);
+    $(".exper__droits__list--right--arrow3").hide();
+  });
+  $(".exper__droits--reduire3").click(function () {
+    $(".exper__droits--detail3").toggle(500);
+    $(".exper__droits__list--right--arrow3").show();
+  });
+});
 },{"../sass/app.scss":"../sass/app.scss"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -221,7 +254,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49745" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61191" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
