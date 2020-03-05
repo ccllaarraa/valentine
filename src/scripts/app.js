18fe9c9768572c540d5f93ce266aecc645c73129
Clara Pasteau <clarapasteau@gmail.com>
2020-03-05 17:59:33 +0100

    scroll functions

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,16 +1,35 @@
 import "../sass/app.scss";
 
-$(document).ready(function(){
+window.addEventListener("scroll", function() {
+    showArrow();
+});
 
+
+
+function showArrow() {
+    var scrollTop = window.pageYOffset;
+    if (scrollTop > 1300 ){
+        document.getElementsByClassName('arrow-up')[0].classList.add('reveal')
+        } else if (scrollTop < 1300 ) {
+            document.getElementsByClassName('arrow-up')[0].classList.remove('reveal')
+    }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+$(document).ready(function(){
     // expertise 1
     $(".exper__droits--detail1").hide();
     $(".exper__droits__list--right--arrow1").click(function(){
-        $(".exper__droits--detail1").toggle(500);
+        $(".exper__droits--detail1").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow1").hide();
       });
 
       $(".exper__droits--reduire1").click(function(){
-        $(".exper__droits--detail1").toggle(500);
+        $(".exper__droits--detail1").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow1").show();
       });
 
@@ -18,25 +37,28 @@ $(document).ready(function(){
     // expertise 2
     $(".exper__droits--detail2").hide();
     $(".exper__droits__list--right--arrow2").click(function(){
-        $(".exper__droits--detail2").toggle(500);
+        $(".exper__droits--detail2").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow2").hide();
       });
 
       $(".exper__droits--reduire2").click(function(){
-        $(".exper__droits--detail2").toggle(500);
+        $(".exper__droits--detail2").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow2").show();
       });
 
     // expertise 3
     $(".exper__droits--detail3").hide();
     $(".exper__droits__list--right--arrow3").click(function(){
-        $(".exper__droits--detail3").toggle(500);
+        $(".exper__droits--detail3").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow3").hide();
       });
 
       $(".exper__droits--reduire3").click(function(){
-        $(".exper__droits--detail3").toggle(500);
+        $(".exper__droits--detail3").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow3").show();
       });
   
   });
+
+
+
--- web/build/dev/app.js
+++ web/build/dev/app.js
@@ -194,35 +194,54 @@ module.hot.accept(reloadCSS);
 
 require("../sass/app.scss");
 
+window.addEventListener("scroll", function () {
+  showArrow();
+});
+
+function showArrow() {
+  var scrollTop = window.pageYOffset;
+
+  if (scrollTop > 1300) {
+    document.getElementsByClassName('arrow-up')[0].classList.add('reveal');
+  } else if (scrollTop < 1300) {
+    document.getElementsByClassName('arrow-up')[0].classList.remove('reveal');
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 $(document).ready(function () {
   // expertise 1
   $(".exper__droits--detail1").hide();
   $(".exper__droits__list--right--arrow1").click(function () {
-    $(".exper__droits--detail1").toggle(500);
+    $(".exper__droits--detail1").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow1").hide();
   });
   $(".exper__droits--reduire1").click(function () {
-    $(".exper__droits--detail1").toggle(500);
+    $(".exper__droits--detail1").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow1").show();
   }); // expertise 2
 
   $(".exper__droits--detail2").hide();
   $(".exper__droits__list--right--arrow2").click(function () {
-    $(".exper__droits--detail2").toggle(500);
+    $(".exper__droits--detail2").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow2").hide();
   });
   $(".exper__droits--reduire2").click(function () {
-    $(".exper__droits--detail2").toggle(500);
+    $(".exper__droits--detail2").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow2").show();
   }); // expertise 3
 
   $(".exper__droits--detail3").hide();
   $(".exper__droits__list--right--arrow3").click(function () {
-    $(".exper__droits--detail3").toggle(500);
+    $(".exper__droits--detail3").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow3").hide();
   });
   $(".exper__droits--reduire3").click(function () {
-    $(".exper__droits--detail3").toggle(500);
+    $(".exper__droits--detail3").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow3").show();
   });
 });
@@ -254,7 +273,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61191" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57077" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
