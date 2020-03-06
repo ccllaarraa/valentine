c8f37110597b8e1f1c05581726aa31b3244fcc93
Clara Pasteau <clarapasteau@gmail.com>
2020-03-06 17:11:22 +0100

    adjust font

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -23,7 +23,7 @@ function topFunction() {
 $(document).ready(function(){
     // expertise 1
     $(".exper__droits--detail1").hide();
-    $(".exper__droits__list--right--arrow1").click(function(){
+    $(".exper__droits__list--right--arrow1, .titre-exp1").click(function(){
         $(".exper__droits--detail1").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow1").hide();
       });
@@ -36,7 +36,7 @@ $(document).ready(function(){
 
     // expertise 2
     $(".exper__droits--detail2").hide();
-    $(".exper__droits__list--right--arrow2").click(function(){
+    $(".exper__droits__list--right--arrow2 , .titre-exp2").click(function(){
         $(".exper__droits--detail2").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow2").hide();
       });
@@ -48,7 +48,7 @@ $(document).ready(function(){
 
     // expertise 3
     $(".exper__droits--detail3").hide();
-    $(".exper__droits__list--right--arrow3").click(function(){
+    $(".exper__droits__list--right--arrow3 , .titre-exp3").click(function(){
         $(".exper__droits--detail3").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow3").hide();
       });
@@ -57,6 +57,28 @@ $(document).ready(function(){
         $(".exper__droits--detail3").toggle(500, 'swing');
         $(".exper__droits__list--right--arrow3").show();
       });
+
+      $(".arrow-up ").click(function() {
+        $("html, body").animate({ 
+            scrollTop: 800 
+        }, "slow");
+        return false;
+    });
+
+    $('.landing__arrow__png').click(function(event) {
+        // Preventing default action of the event
+        event.preventDefault();
+        // Getting the height of the document
+        var n = 800;
+        $('html, body').animate({ scrollTop: n }, 'slow');
+    });
+
+    $(document).on('click', 'a[href^="#"]', function (e) {
+        e.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 'slow');
+    });
   
   });
 
--- web/build/dev/app.js
+++ web/build/dev/app.js
@@ -216,7 +216,7 @@ function topFunction() {
 $(document).ready(function () {
   // expertise 1
   $(".exper__droits--detail1").hide();
-  $(".exper__droits__list--right--arrow1").click(function () {
+  $(".exper__droits__list--right--arrow1, .titre-exp1").click(function () {
     $(".exper__droits--detail1").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow1").hide();
   });
@@ -226,7 +226,7 @@ $(document).ready(function () {
   }); // expertise 2
 
   $(".exper__droits--detail2").hide();
-  $(".exper__droits__list--right--arrow2").click(function () {
+  $(".exper__droits__list--right--arrow2 , .titre-exp2").click(function () {
     $(".exper__droits--detail2").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow2").hide();
   });
@@ -236,13 +236,34 @@ $(document).ready(function () {
   }); // expertise 3
 
   $(".exper__droits--detail3").hide();
-  $(".exper__droits__list--right--arrow3").click(function () {
+  $(".exper__droits__list--right--arrow3 , .titre-exp3").click(function () {
     $(".exper__droits--detail3").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow3").hide();
   });
   $(".exper__droits--reduire3").click(function () {
     $(".exper__droits--detail3").toggle(500, 'swing');
     $(".exper__droits__list--right--arrow3").show();
+  });
+  $(".arrow-up ").click(function () {
+    $("html, body").animate({
+      scrollTop: 800
+    }, "slow");
+    return false;
+  });
+  $('.landing__arrow__png').click(function (event) {
+    // Preventing default action of the event
+    event.preventDefault(); // Getting the height of the document
+
+    var n = 800;
+    $('html, body').animate({
+      scrollTop: n
+    }, 'slow');
+  });
+  $(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 'slow');
   });
 });
 },{"../sass/app.scss":"../sass/app.scss"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
